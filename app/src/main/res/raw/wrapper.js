ea9c4ba4a38ad2116aa3a1a0c491d23abcfef3f2
Zach Deibert <zachdeibert@gmail.com>
2023-05-06 13:34:31 -0500

    Made it automatically full-screen

--- app/src/main/res/raw/wrapper.js
+++ app/src/main/res/raw/wrapper.js
@@ -1,8 +1,27 @@
 "use strict";
 
-setInterval(function() {
-    var tags = document.getElementsByTagName("h1");
-    if (tags.length > 0) {
-        tags[0].innerText = "Hello, world!";
-    }
-}, 1000);
+(function () {
+    var checkForLoadComplete = [];
+    checkForLoadComplete[0] = function() {
+        var iframes = document.getElementsByTagName("iframe");
+        if (iframes.length > 0) {
+            var style = document.createElement("style");
+            style.innerText =
+                "* {" +
+                    "visibility: hidden !important;" +
+                "}" +
+                "iframe {" +
+                    "visibility: visible !important;" +
+                    "position: fixed;" +
+                    "top: 0;" +
+                    "left: 0;" +
+                    "bottom: 0;" +
+                    "right: 0;" +
+                "}";
+            document.body.appendChild(style);
+        } else {
+            setTimeout(checkForLoadComplete[0], 100);
+        }
+    };
+    checkForLoadComplete[0]();
+})();
